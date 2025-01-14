e730e4294f02a697755eed435cb0c97065514a19
CarlaMSilva <carlamariasilva41@outlook.com>
2025-01-14 14:10:30 -0300

    1o commit

---
+++ src/test/java/br/edu/ifpe/recife/gcms/WebApp/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HomeControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
@@ -0,0 +1,18 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Controller; 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
