28fe069e0d0c29116742b7ca7ce0d973e93fd4fb
CarlaMSilva <carlamariasilva41@outlook.com>
2025-01-28 14:22:24 -0300

    Tentativa de correção

--- src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -1,18 +1,20 @@
 package br.edu.ifpe.recife.gcms.WebApp;
 
-import org.springframework.stereotype.Controller; 
-import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;  // Importando a anotação @GetMapping
 
 @Controller
 public class HomeController { 
-	@RequestMapping("/Carla") 
-	public @ResponseBody String greeting() { 
-		return "Hello, World! Carla"; 
-		System.out.println("Modificando o cód");
-		} 
-@GetMapping("/hotfix")
- public String hotfix() {
-         return "This is a hotfix!";
-     }
-	}
+    @RequestMapping("/Carla")
+    public @ResponseBody String greeting() {
+        System.out.println("Modificando o cód");  // Agora será executado
+        return "Hello, World! Carla"; 
+    }
+
+    @GetMapping("/hotfix")
+    public String hotfix() {
+        return "This is a hotfix!";
+    }
+}
