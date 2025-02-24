2375559ad1eb1ec0ae6bb63d5d59365c0fd8fc5f
CarlaMSilva <carlamariasilva41@outlook.com>
2025-02-24 17:21:55 -0300

    Pratica 2 parte Hello world

---
+++ src/main/java/br/edu/ifpe/recife/gcms/Pratica02/HomeController.java
@@ -0,0 +1,16 @@
+package br.edu.ifpe.recife.gcms.Pratica02;
+
+	import org.springframework.stereotype.Controller;
+	import org.springframework.web.bind.annotation.RequestMapping; 
+	import org.springframework.web.bind.annotation.ResponseBody;
+	
+	@Controller 
+	public class HomeController {
+		
+		@RequestMapping("/") 
+		public @ResponseBody String greeting() {
+		return "Hello, World!";
+		}
+	}
+
+
---
+++ src/test/java/br/edu/ifpe/recife/gcms/Pratica02/Pratica02ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.recife.gcms.Pratica02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratica02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
